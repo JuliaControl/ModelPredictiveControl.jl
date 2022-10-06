06e9bdb4b05702e1f13f34dfc7cad0964cac9cbb
Francis Gagnon <franckgaga2@gmail.com>
2022-10-06 11:37:59 -0400

    Project starting files

---
+++ example/juMPC.jl
@@ -0,0 +1,33 @@
+
+using DAQP
+using OSQP
+using JuMP, LinearAlgebra
+using MAT
+
+vars_ml = matread("matlab.mat")
+
+H_qp = vars_ml["mMPC"]["Hqp"]
+f_qp = vec(vars_ml["fqp"])
+
+A = vars_ml["A"]
+b = vec(vars_ml["b"])
+
+ΔUϵ_min = vec(vars_ml["mMPC"]["DU_min"])
+ΔUϵ_max = vec(vars_ml["mMPC"]["DU_max"])
+
+Hc = Int(vars_ml["mMPC"]["Hc"])
+nu = Int(vars_ml["mMPC"]["nu"])
+n_var = (Hc*nu)+1
+
+model = Model(OSQP.Optimizer)
+
+@variable(model, ΔUϵ_min[i] .<= ΔUϵ[i=1:n_var] .<= ΔUϵ_max[i])
+
+@objective(model, Min, 0.5*(ΔUϵ)'H_qp*(ΔUϵ) + f_qp'*(ΔUϵ))
+@constraint(model, A*ΔUϵ .<= b)
+optimize!(model)
+
+julia_ΔUϵ = value.(ΔUϵ)
+
+ml_ΔUϵ = vec(vars_ml["deltaUhc"])
+julia_ΔUϵ - ml_ΔUϵ
