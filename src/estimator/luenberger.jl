e3a871fefbdc1516236332384c03cbab8e38468b
franckgaga <franckgaga2@gmail.com>
2023-02-10 16:45:30 -0500

    start of luenberger observer

---
+++ src/estimator/luenberger.jl
@@ -0,0 +1,45 @@
+struct Luenberger <: StateEstimator
+    model::LinModel
+    x̂::Vector{Float64}
+    i_ym::IntRangeOrVector
+    nx̂::Int
+    nym::Int
+    nyu::Int
+    nxs::Int
+    As::Matrix{Float64}
+    Cs::Matrix{Float64}
+    nint_ym::Vector{Int}
+    Â   ::Matrix{Float64}
+    B̂u  ::Matrix{Float64}
+    B̂d  ::Matrix{Float64}
+    Ĉ   ::Matrix{Float64}
+    D̂d  ::Matrix{Float64}
+    Ĉm  ::Matrix{Float64}
+    D̂dm ::Matrix{Float64}
+    f̂::Function
+    ĥ::Function
+    K::Matrix{Float64}
+    function Luenberger(model, i_ym, nint_ym, Asm, Csm, L)
+        nx, ny = model.nx, model.ny
+        nym, nyu = length(i_ym), ny - length(i_ym)
+        nxs = size(Asm,1)
+        nx̂ = nx + nxs
+        validate_kfcov(nym, nx̂, Q̂, R̂)
+        As, _ , Cs, _  = stoch_ym2y(model, i_ym, Asm, [], Csm, [])
+        f̂, ĥ, Â, B̂u, Ĉ, B̂d, D̂d = augment_model(model, As, Cs)
+        Ĉm, D̂dm = Ĉ[i_ym, :], D̂d[i_ym, :] # measured outputs ym only
+        K = L
+        x̂ = [copy(model.x); zeros(nxs)]
+        return new(
+            model, 
+            x̂,
+            i_ym, nx̂, nym, nyu, nxs, 
+            As, Cs, nint_ym,
+            Â, B̂u, B̂d, Ĉ, D̂d, 
+            Ĉm, D̂dm,
+            f̂, ĥ,
+            Q̂, R̂,
+            K
+        )
+    end
+end
