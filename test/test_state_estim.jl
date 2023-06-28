a49db53c34f61c0a6c201d1f6bc855ef854ee380
franckgaga <franckgaga2@gmail.com>
2023-06-28 16:50:04 -0400

    debug test julia 1.6

--- test/test_state_estim.jl
+++ test/test_state_estim.jl
@@ -33,7 +33,7 @@ sys = [ tf(1.90,[18.0,1])   tf(1.90,[18.0,1])   tf(1.90,[18.0,1]);
     @test_throws ErrorException SteadyKalmanFilter(linmodel1, nint_ym=[-1,0])
     @test_throws ErrorException SteadyKalmanFilter(linmodel1, nint_ym=0, σQ=[1])
     @test_throws ErrorException SteadyKalmanFilter(linmodel1, nint_ym=0, σR=[1,1,1])
-    model_unobs = LinModel([1 0;0 1.5], [1; 0;;], [1 0], zeros(2,0), zeros(1,0),1,1,2,1,0)
+    model_unobs = LinModel([1 0;0 1.5], [1;0][:,:], [1 0], zeros(2,0), zeros(1,0),1,1,2,1,0)
     @test_throws ErrorException SteadyKalmanFilter(model_unobs, nint_ym=[1])
 end
     
