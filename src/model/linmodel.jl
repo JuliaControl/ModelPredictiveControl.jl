6ea9907a08aba74e598d960db2018c7f3f796ad7
franckgaga <franckgaga2@gmail.com>
2024-04-12 15:58:32 -0400

    minor doc correction

--- src/model/linmodel.jl
+++ src/model/linmodel.jl
@@ -192,7 +192,7 @@ end
 Construct the model from the discrete state-space matrices `A, Bu, C, Bd, Dd` directly.
 
 See [`LinModel(::StateSpace)`](@ref) Extended Help for the meaning of the matrices. The
-arguments `Bd` and `Dd` can be the scalar `0` is there is no measured disturbances. This
+arguments `Bd` and `Dd` can be the scalar `0` if there is no measured disturbance. This
 syntax do not modify the state-space representation provided in argument (`minreal` is not
 called). Care must be taken to ensure that the model is controllable and observable. The 
 optional parameter `NT` explicitly set the number type of vectors (default to `Float64`).
