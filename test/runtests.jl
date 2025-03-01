90ad3b3c10a741515cb69d2e8cec72240538beb9
franckgaga <franckgaga2@gmail.com>
2025-03-01 02:59:57 -0500

    test: doctests in `@testitem`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,5 @@
-# spell-checker: disable
-
 using ModelPredictiveControl
-using Documenter
-using Test, TestItemRunner
+using Test, TestItems, TestItemRunner
 
 @run_package_tests(verbose=true)
 
@@ -11,19 +8,6 @@ include("1_test_sim_model.jl")
 include("2_test_state_estim.jl")
 include("3_test_predictive_control.jl")
 include("4_test_plot_sim.jl")
-
-old_debug_level = get(ENV, "JULIA_DEBUG", "")
-DocMeta.setdocmeta!(
-    ModelPredictiveControl, 
-    :DocTestSetup, 
-    :(
-        using ModelPredictiveControl, ControlSystemsBase;
-        ENV["JULIA_DEBUG"] = ""; # temporarily disable @debug logging for the doctests
-    ); 
-    recursive=true,
-    warn=false
-)
-doctest(ModelPredictiveControl, testset="DocTest")
-ENV["JULIA_DEBUG"] = old_debug_level
+include("5_test_doctest.jl")
 
 nothing
