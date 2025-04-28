b91368ec1866dcdc8f7308cbf5b00a99a9bf4972
franckgaga <franckgaga2@gmail.com>
2025-04-28 12:39:07 -0400

    doc: correct ext ref

--- src/controller/nonlinmpc.jl
+++ src/controller/nonlinmpc.jl
@@ -566,7 +566,7 @@ This method is really intricate and I'm not proud of it. That's because of 3 ele
 
 - These functions are used inside the nonlinear optimization, so they must be type-stable
   and as efficient as possible. All the function outputs and derivatives are cached and
-  updated in-place if required to use the efficient [`value_and_jacobian!`](@extref DifferentiationInterface value_and_jacobian!)`.
+  updated in-place if required to use the efficient [`value_and_jacobian!`](@extref DifferentiationInterface DifferentiationInterface.value_and_jacobian!)`.
 - The `JuMP` NLP syntax forces splatting for the decision variable, which implies use
   of `Vararg{T,N}` (see the [performance tip][@extref Julia Be-aware-of-when-Julia-avoids-specializing]
   ) and memoization to avoid redundant computations. This is already complex, but it's even
