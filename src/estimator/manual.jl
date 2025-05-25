ebf6f9b49951e42127b1347b53af4b6efcc9869c
franckgaga <franckgaga2@gmail.com>
2025-05-25 14:54:46 -0400

    doc: debug doc test

--- src/estimator/manual.jl
+++ src/estimator/manual.jl
@@ -95,31 +95,31 @@ ManualEstimator estimator with a sample time Ts = 0.5 s, LinModel and:
     the minimal required information to construct any [`PredictiveController`](@ref) object:
 
     ```jldoctest
-    julia> function man_sim!()
-        f(x,u,_,_) = 0.5*sin.(x + u)
-        h(x,_,_) = x
-        model = NonLinModel(f, h, 10.0, 1, 1, 1, solver=nothing)
-        linModel = linearize(model, x=[0], u=[0])
-        man = ManualEstimator(linModel, nint_u=[1])
-        mpc = LinMPC(man)
-        estim = MovingHorizonEstimator(model, nint_u=[1], He=5)
-        estim = setconstraint!(estim, v̂min=[-0.001], v̂max=[0.001])
-        initstate!(estim, [0], [0])
-        y_data, ŷ_data = zeros(5), zeros(5)
-        for i=1:5
-            y = model()                         # simulated measurement
-            x̂ = preparestate!(estim, y)         # correct nonlinear MHE state estimate
-            ŷ = estim()                         # nonlinear MHE estimated output
-            setstate!(mpc, x̂)                   # update MPC with the MHE corrected state 
-            u = moveinput!(mpc, [0])
-            y_data[i], ŷ_data[i] = y[1], ŷ[1]
-            updatestate!(estim, u, y)           # update nonlinear MHE estimation
-            updatestate!(model, u .+ 0.5)       # update simulator with load disturbance
-        end
-        return collect([y_data ŷ_data]')
-    end
+    julia> function man_sim()
+               f(x,u,_,_) = 0.5*sin.(x + u)
+               h(x,_,_) = x
+               model = NonLinModel(f, h, 10.0, 1, 1, 1, solver=nothing)
+               linModel = linearize(model, x=[0], u=[0])
+               man = ManualEstimator(linModel, nint_u=[1])
+               mpc = LinMPC(man)
+               estim = MovingHorizonEstimator(model, nint_u=[1], He=5)
+               estim = setconstraint!(estim, v̂min=[-0.001], v̂max=[0.001])
+               initstate!(estim, [0], [0])
+               y_data, ŷ_data = zeros(5), zeros(5)
+               for i=1:5
+                   y = model()                     # simulated measurement
+                   x̂ = preparestate!(estim, y)     # correct nonlinear MHE state estimate
+                   ŷ = estim()                     # nonlinear MHE estimated output
+                   setstate!(mpc, x̂)               # update MPC with the MHE corrected state 
+                   u = moveinput!(mpc, [0])
+                   y_data[i], ŷ_data[i] = y[1], ŷ[1]
+                   updatestate!(estim, u, y)       # update nonlinear MHE estimation
+                   updatestate!(model, u .+ 0.5)   # update simulator with load disturbance
+               end
+               return collect([y_data ŷ_data]')
+           end;
 
-    julia> YandŶ = man_sim!()
+    julia> YandŶ = man_sim()
     2×5 Matrix{Float64}:
       0.0          0.239713  0.227556  0.157837  0.0986288
      -1.41242e-19  0.238713  0.226556  0.156837  0.0976288
