79f46159e2cb2ac5ce02d4b4e540572842867480
franckgaga <franckgaga2@gmail.com>
2025-06-29 11:00:31 -0400

    bench: add NonLinModel no-allocation tests

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,22 +1,43 @@
 using BenchmarkTools
 
-using ModelPredictiveControl, ControlSystemsBase
+using ModelPredictiveControl, ControlSystemsBase, LinearAlgebra
 Ts = 400.0
 sys = [ tf(1.90,[1800.0,1])   tf(1.90,[1800.0,1])   tf(1.90,[1800.0,1]);
         tf(-0.74,[800.0,1])   tf(0.74,[800.0,1])    tf(-0.74,[800.0,1])   ] 
 linmodel = setop!(LinModel(sys, Ts, i_d=[3]), uop=[10,50], yop=[50,30], dop=[5])
+function f!(ẋ, x, u, d, p)
+    mul!(ẋ, p.A, x)
+    mul!(ẋ, p.Bu, u, 1, 1)
+    mul!(ẋ, p.Bd, d, 1, 1)
+    return nothing
+end
+function h!(y, x, d, p)
+    mul!(y, p.C, x)
+    mul!(y, p.Dd, d, 1, 1)
+    return nothing
+end
+nonlinmodel = NonLinModel(f!, h!, Ts, 2, 4, 2, 1, p=linmodel, solver=nothing)
+nonlinmodel = setop!(nonlinmodel, uop=[10,50], yop=[50,30], dop=[5])
 u, d = [10, 50], [5]
 
 const SUITE = BenchmarkGroup()
 
 ## ================== SimModel benchmarks =========================================
 SUITE["SimModel"]["allocation"] = BenchmarkGroup(["allocation"])
-SUITE["SimModel"]["allocation"]["updatestate!"] = @benchmarkable(
+SUITE["SimModel"]["allocation"]["LinModel_updatestate"] = @benchmarkable(
     updatestate!($linmodel, $u, $d),
     samples=1
 )
-SUITE["SimModel"]["allocation"]["evaloutput"] = @benchmarkable(
+SUITE["SimModel"]["allocation"]["LinModel_evaloutput"] = @benchmarkable(
     evaloutput($linmodel, $d),
+    samples=1
+)
+SUITE["SimModel"]["allocation"]["NonLinModel_updatestate"] = @benchmarkable(
+    updatestate!($nonlinmodel, $u, $d),
+    samples=1
+)
+SUITE["SimModel"]["allocation"]["NonLinModel_evaloutput"] = @benchmarkable(
+    evaloutput($nonlinmodel, $d),
     samples=1
 )
 
