a954c3fcf0fca9a4ff2cc1f07322f3341b79ca9c
franckgaga <franckgaga2@gmail.com>
2026-01-18 15:58:29 -0500

    test: `LinearMPC` with hard bounds and tight tolerances

--- test/5_test_extensions.jl
+++ test/5_test_extensions.jl
@@ -12,13 +12,11 @@
         u1 = [20.0, 20.0]
         u2 = [20.0, 20.0]
         model.x0 .= 0
-        y_data = zeros(model.ny, N)
         u_data1, u_data2 = zeros(model.nu, N), zeros(model.nu, N)
         for k in 0:N-1
             k == 10 && (r .= [45; 30.0])
             k == 25 && (r .= [50; 45.0])
             y = model()
-            y_data[:, k+1] = y
             preparestate!(mpc1, y)
             x̂ = LinearMPC.correct_state!(mpc2, y)
             u1 = moveinput!(mpc1, r)
@@ -28,11 +26,19 @@
             updatestate!(mpc1, u1, y)
             LinearMPC.predict_state!(mpc2, u2)
         end
-        return y_data, u_data1, u_data2
+        return u_data1, u_data2
     end
     N = 50
-    y_data, u_data1, u_data2 = sim_both(model, mpc1, mpc2, N)
-    @test u_data1 ≈ u_data2 atol=1e-3 rtol=1e-3
+    u_data1, u_data2 = sim_both(model, mpc1, mpc2, N)
+    @test u_data1 ≈ u_data2 atol=1e-3 rtol=1e-3 # looser tol due to different softening
+
+    mpc1_hard = LinMPC(model, Hp=15, Cwt=Inf, optim=optim)
+    mpc1_hard = setconstraint!(mpc1_hard, ymin=[48, -Inf], umax=[Inf, 30])
+    mpc2_hard = LinearMPC.MPC(mpc1_hard)
+    u_data1_hard, u_data2_hard = sim_both(
+        model, mpc1_hard, mpc2_hard, N
+    )
+    @test u_data1_hard ≈ u_data2_hard atol=1e-12 # exact match for hard constraints
 
     mpc_ms = LinMPC(model; transcription=MultipleShooting(), optim)
     @test_throws ErrorException LinearMPC.MPC(mpc_ms)
