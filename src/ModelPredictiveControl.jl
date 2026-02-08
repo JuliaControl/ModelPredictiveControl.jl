04ea3d1c046c1eac5040451b66699abacf727e63
franckgaga <franckgaga2@gmail.com>
2026-02-08 10:52:35 -0500

    debug: avoid duplicate precompile macros

--- src/ModelPredictiveControl.jl
+++ src/ModelPredictiveControl.jl
@@ -57,14 +57,8 @@ include("state_estim.jl")
 include("predictive_control.jl")
 include("plot_sim.jl")
 
-@setup_workload begin
-    # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the
-    # size of the precompile file and potentially make loading faster.
-    @compile_workload begin
-        # all calls in this block will be precompiled, regardless of whether
-        # they belong to your package or not (on Julia 1.8 and higher)
-        include("precompile.jl")
-    end
-end
+
+include("precompile.jl")
+
 
 end
