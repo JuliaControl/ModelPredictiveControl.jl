2493daa8a4c8f3080439a86a08dc8e0ece2196e2
franckgaga <franckgaga2@gmail.com>
2026-03-03 11:03:08 -0500

    test: unit case with `OrthogonalCollocation`

--- test/3_test_predictive_control.jl
+++ test/3_test_predictive_control.jl
@@ -993,8 +993,14 @@ end
     preparestate!(nmpc5_1, [0.0])
     u = moveinput!(nmpc5_1, [1/0.001])
     @test u ≈ [1.0] atol=5e-2
-    nonlinmodel2 = NonLinModel{Float32}(f, h, 3000.0, 1, 2, 1, 1, solver=nothing, p=linmodel2)
+
+    transcription = OrthogonalCollocation(0, 4)
+    nmpc6 = NonLinMPC(InternalModel(nonlinmodel_c); Nwt=[0], Hp=100, Hc=1, transcription)
+    preparestate!(nmpc6, [0.0])
+    u = moveinput!(nmpc6, [1/0.001])
+    @test u ≈ [1.0] atol=5e-2
     
+    nonlinmodel2 = NonLinModel{Float32}(f, h, 3000.0, 1, 2, 1, 1, solver=nothing, p=linmodel2)
     nmpc7  = NonLinMPC(nonlinmodel2, Hp=10)
     y = similar(nonlinmodel2.yop)
     ModelPredictiveControl.h!(y, nonlinmodel2, Float32[0,0], Float32[0], nonlinmodel2.p)
