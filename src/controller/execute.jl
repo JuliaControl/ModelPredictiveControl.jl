10df028625105d7c533cd1af6d34b2b14d7e5b6e
franckgaga <franckgaga2@gmail.com>
2026-03-04 09:49:11 -0500

    added: extract `ϵ` in `obj_nonlinprog!`

--- src/controller/execute.jl
+++ src/controller/execute.jl
@@ -185,13 +185,16 @@ function getinfo(mpc::PredictiveController{NT}) where NT<:Real
     return info
 end
 
-"""
-    getϵ(mpc::PredictiveController, Z̃) -> ϵ
+@dow raw"""
+    getϵ(mpc::PredictiveController, Z̃orΔŨ) -> ϵ
 
-Get the slack `ϵ` from the decision vector `Z̃` if present, otherwise return 0.
+Get the slack `ϵ` from `Z̃orΔŨ` if present, otherwise return 0.
+
+The argument `Z̃orΔŨ` can be the augmented decision vector ``\mathbf{Z̃}`` or the augmented
+input increment vector ``\mathbf{ΔŨ}``, it works with both.
 """
-function getϵ(mpc::PredictiveController, Z̃::AbstractVector{NT}) where NT<:Real
-    return mpc.nϵ ≠ 0 ? Z̃[end] : zero(NT)
+function getϵ(mpc::PredictiveController, Z̃orΔŨ::AbstractVector{NT}) where NT<:Real
+    return mpc.nϵ ≠ 0 ? Z̃orΔŨ[end] : zero(NT)
 end
 
 """
@@ -425,7 +428,8 @@ function obj_nonlinprog!(
         JR̂u = dot(Ū, mpc.weights.L_Hp, Ū)
     end
     # --- economic term ---
-    E_JE = obj_econ(mpc, model, Ue, Ŷe)
+    ϵ = getϵ(mpc, ΔŨ)
+    E_JE = obj_econ(mpc, model, Ue, Ŷe, ϵ)
     return JR̂y + JΔŨ + JR̂u + E_JE
 end
 
