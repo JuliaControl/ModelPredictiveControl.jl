0931e79de20708bc297d3d78ee08f1c357faa7ef
franckgaga <franckgaga2@gmail.com>
2026-03-11 11:24:08 -0400

    debug: correct type

--- src/controller/transcription.jl
+++ src/controller/transcription.jl
@@ -1070,7 +1070,7 @@ end
 linconstrainteq!(::PredictiveController, ::SimModel, ::InternalModel, ::TranscriptionMethod) = nothing
 "No linear equality constraints for [`SingleShooting`(@ref) (N/A).]"
 linconstrainteq!(::PredictiveController, ::SimModel, ::StateEstimator, ::SingleShooting)     = nothing
-linconstrainteq!(::PredictiveController, ::SimModel, ::IntenalModel,   ::SingleShooting)     = nothing
+linconstrainteq!(::PredictiveController, ::SimModel, ::InternalModel,  ::SingleShooting)     = nothing
 
 @doc raw"""
     set_warmstart!(mpc::PredictiveController, ::SingleShooting, Z̃var) -> Z̃s
