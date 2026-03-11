a52f1511a5bde1082bbce4621add03a9521b67f7
franckgaga <franckgaga2@gmail.com>
2026-03-11 11:57:23 -0400

    doc: minor correction

--- src/controller/transcription.jl
+++ src/controller/transcription.jl
@@ -1556,7 +1556,7 @@ the model dynamics are computed by:
 ```
 for ``j = 0, 1, ... , H_p-1``, and knowing that the ``\mathbf{k}_i(k+j)`` vectors are
 extracted from the decision variable `Z̃`. The ``\mathbf{x̂_d}`` vectors are the
-deterministic states extracted from ``\mathbf{X̂_̂0}`` also in `Z̃`, and they correspond to the
+deterministic states extracted from ``\mathbf{X̂_0}`` also in `Z̃`, and they correspond to the
 states at the beginning of the interval ``τ_0=0``. The ``\mathbf{k̇}_i`` derivative for the
 ``i``th collocation point is computed from the continuous-time function `model.f!` and:
 ```math
