a52c6e5c1b264b686d9cb337f551ad07b1b30c99
franckgaga <franckgaga2@gmail.com>
2026-03-24 16:29:24 -0400

    test: add `Aqua` The test exclude `unbound_args` since `Vararg` is needed for the splatting syntax of `JuMP.@operator`, and JuMP will never call the associated functions with 0 argument (it would mean 0 decision variable).

---
+++ test/7_test_aqua.jl
@@ -0,0 +1,9 @@
+@testitem "Aqua" begin
+    using Aqua
+    # All the functions defined inside `ModelPredictiveControl.get_nonlinobj_op` with the 
+    # `Vararg`s have unbound type parameters. This is necessary for the splatting syntax of
+    # `JuMP.@operator`, and JuMP will never call these functions with 0 argument, so
+    # defining zero-argument methods would be useless, so I disable this check here.
+    unbound_args = false
+    Aqua.test_all(ModelPredictiveControl, unbound_args)
+end
