6f6643c31005cf6def2c1497ac63b62ef5f29b82
franckgaga <franckgaga2@gmail.com>
2026-07-14 14:26:10 -0400

    changed: new general `transcription.jl` file

--- src/ModelPredictiveControl.jl
+++ src/ModelPredictiveControl.jl
@@ -58,6 +58,7 @@ export SimResult, getinfo, sim!
 
 include("general.jl")
 include("sim_model.jl")
+include("transcription.jl")
 include("state_estim.jl")
 include("predictive_control.jl")
 include("plot_sim.jl")
