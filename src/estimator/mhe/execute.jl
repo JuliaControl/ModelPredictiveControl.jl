94e254ae82602bf635d609e265b7e3eb724b7d2e
franckgaga <franckgaga2@gmail.com>
2026-08-05 15:21:34 -0400

    debug: new signature for `init_defectmat_mhe`

--- src/estimator/mhe/execute.jl
+++ src/estimator/mhe/execute.jl
@@ -981,8 +981,8 @@ function setmodel_estimator!(
     con.BX̂ .= BX̂
     # --- defect matrices ---
     ES, GS, JS, BS = init_defectmat_mhe(
-        model, transcription, He, estim.Â, estim.B̂u, estim.B̂d, estim.x̂op, estim.f̂op, 
-        estim.direct
+        model, transcription, He, 
+        estim.Â, estim.B̂u, estim.B̂d, estim.x̂op, estim.f̂op, estim.As, estim.direct
     )
     Aeq, ẼS = augmentdefect(ES, nε; slackfirst=true)
     con.ẼS .= ẼS
